96801728208255fe4774f1dfdce7433f047282e1
luiz-antonio05 <137508484+luiz-antonio05@users.noreply.github.com>
2025-06-20 22:11:11 +0000

    Inicio codigo nivel novato do jogo de xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+//int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -28,5 +28,49 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
+    //return 0;
+
+
+#include <stdio.h>
+
+int main() {
+  // simulação de movimento: Torre
+  // Movimento: 5 casas para a direita
+  // Estrutura: for
+
+  printf("Movimento da Torre: \n");
+
+  for(int i = 1; i <= 5; i++)
+  {
+    printf("Direita\n");
+  }
+
+  // simulação de movimento: Bispo
+  // Movimento: 5 casas na diagonal cima-direita
+  // Estrutura: while
+
+  printf("\nMovimento do Bispo: \n");
+
+  int contBispo = 1;
+  while(contBispo <= 5)
+  {
+    printf("Cima-Direita\n");
+    contBispo++;
+  }
+
+  // Simulação do movimento da RAINHA
+  // Movimento: 8 casas para a esquerda
+  // Estrutura usada: do-while
+
+  printf("\nMovimento da Rainha: \n");
+
+  int contRainha = 1;
+
+  do{
+    printf("Esquerda\n");
+    contRainha++;
+  } while(contRainha <= 8);
+
+  return 0;
+
 }
